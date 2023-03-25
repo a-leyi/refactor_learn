3acfa0ea610cb93658a7fc7cecb5579713fad8e0
李泽锴 <57347361@qq.com>
2023-03-26 01:09:21 +0800

    update tests

--- src/test/java/com/leyi/refactor/RefactorApplicationTests.java
+++ src/test/java/com/leyi/refactor/RefactorApplicationTests.java
@@ -9,21 +9,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Vector;
 
 @Slf4j
 @SpringBootTest
 class RefactorApplicationTests {
 
-    private static String CONTENT = "Rental Record for 乐一\n" +
-            "\t重构 改善既有代码的设计\t30.0\n" +
-            "Amount owed is 30.0\n" +
-            "You earned 2 frequent renter points";
-
     @Test
     void contextLoads() {
 
+        newReleaseTest();
+        regularTest();
+        childrenTest();
+    }
+
+    private static void newReleaseTest() {
         Movie movie = Movie.builder().title("重构 改善既有代码的设计").priceCode(Movie.NEW_RELEASE).build();
 
         Rental rental = Rental.builder().movie(movie).daysRented(10).build();
@@ -32,11 +32,30 @@ class RefactorApplicationTests {
 
         Customer customer = Customer.builder().name("乐一").rentals(new Vector<>(rentals)).build();
 
-        boolean result = Objects.equals(CONTENT, customer.statement());
-        log.info("result: {}", result);
+        log.info("newReleaseTest statement: {}", customer.statement());
+    }
 
-        if (!result) {
-            throw new RuntimeException("测试未通过");
-        }
+    private static void regularTest() {
+        Movie movie = Movie.builder().title("重构 改善既有代码的设计").priceCode(Movie.REGULAR).build();
+
+        Rental rental = Rental.builder().movie(movie).daysRented(10).build();
+
+        ArrayList<Rental> rentals = Lists.newArrayList(rental);
+
+        Customer customer = Customer.builder().name("乐一").rentals(new Vector<>(rentals)).build();
+
+        log.info("regularTest statement: {}", customer.statement());
+    }
+
+    private static void childrenTest() {
+        Movie movie = Movie.builder().title("重构 改善既有代码的设计").priceCode(Movie.CHILDRENS).build();
+
+        Rental rental = Rental.builder().movie(movie).daysRented(10).build();
+
+        ArrayList<Rental> rentals = Lists.newArrayList(rental);
+
+        Customer customer = Customer.builder().name("乐一").rentals(new Vector<>(rentals)).build();
+
+        log.info("childrenTest statement: {}", customer.statement());
     }
 }
