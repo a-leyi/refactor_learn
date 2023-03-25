c986dda7767f54c943b52ff7f12ab87586d1aa84
李泽锴 <57347361@qq.com>
2023-03-25 23:59:13 +0800

    feat-1.0 init refactor code

--- src/main/java/com/leyi/refactor/firstcase/Customer.java
+++ src/main/java/com/leyi/refactor/firstcase/Customer.java
@@ -18,37 +18,38 @@ public class Customer {
     private Vector<Rental> rentals = new Vector<>();
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration<Rental> rentals = this.rentals.elements();
-        String result = "Rental Record for " + getName() + "\n";
+        return "Rental Record for " + getName() + "\n" + getAllTitle(rentals) +
+                "Amount owed is " + getTotalCharge() + "\n" +
+                "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
+    }
+
+    private StringBuilder getAllTitle(Enumeration<Rental> rentals) {
+        StringBuilder result = new StringBuilder();
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = rentals.nextElement();
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR -> {
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2) thisAmount += (each.getDaysRented() - 2) * 1.5;
-                }
-                case Movie.NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
-                case Movie.CHILDRENS -> {
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3) thisAmount += (each.getDaysRented() - 3) * 1.5;
-                }
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result.append("\t").append(each.getMovie().getTitle()).append("\t").append(each.getCharge()).append("\n");
         }
-        //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration<Rental> rentals = getRentals().elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration<Rental> rentals = getRentals().elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
         return result;
     }
 }
--- src/main/java/com/leyi/refactor/firstcase/Rental.java
+++ src/main/java/com/leyi/refactor/firstcase/Rental.java
@@ -12,5 +12,29 @@ import lombok.NoArgsConstructor;
 public class Rental {
 
     private Movie movie;
+
     private int daysRented;
+
+    public double getCharge() {
+        double thisAmount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR -> {
+                thisAmount += 2;
+                if (getDaysRented() > 2) thisAmount += (getDaysRented() - 2) * 1.5;
+            }
+            case Movie.NEW_RELEASE -> thisAmount += getDaysRented() * 3;
+            case Movie.CHILDRENS -> {
+                thisAmount += 1.5;
+                if (getDaysRented() > 3) thisAmount += (getDaysRented() - 3) * 1.5;
+            }
+        }
+        return thisAmount;
+    }
+
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
